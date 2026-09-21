fe4f661e61602786f78503e1a5121a6e24536aed
Kanak Thakur <bthakur1276@gmail.com>
2026-09-21 13:50:12 +0530

    Time: 5 ms (83.47%), Space: 45.9 MB (71.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int xo=x;
+        int rev=0;
+        while(x>0){
+            int r=x%10;
+            rev=rev*10+r;
+            x=x/10;
+        }
+        return xo==rev;
+        }
+        }
+        
+    
