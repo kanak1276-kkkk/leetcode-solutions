2bf12041f7156c130656ccdef094dd72d308a1e7
Kanak Thakur <bthakur1276@gmail.com>
2026-09-22 10:54:57 +0530

    Time: 1 ms (99.8%), Space: 43.9 MB (67.81%) - LeetHub

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] shortestToChar(String s, char c){
+
+        int n = s.length();
+        int[] answer = new int[n];
+
+        int distance = n;
+
+        // Left to right
+        for (int i = 0; i < n; i++) {
+
+            if (s.charAt(i) == c) {
+                distance = 0;
+            } else {
+                distance++;
+            }
+
+            answer[i] = distance;
+        }
+
+        distance = n;
+
+        // Right to left
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (s.charAt(i) == c) {
+                distance = 0;
+            } else {
+                distance++;
+            }
+
+            answer[i] = Math.min(answer[i], distance);
+        }
+
+        return answer;
+    }
+}
